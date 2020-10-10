eb7694f4b16e70c1643cc90728d5952aab40ef3c
Glitch (hello-webpage) <none>
2020-10-10 10:27:58 +0000

    🎪👮 Checkpoint ./style.css:22133009/335 ./index.html:22133009/42 ./script.js:22133009/663

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const navSlide = () => {
+    const navLinkButton = document.querySelector('.nav-links-button');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('.nav-links li');
+  
+    nav-links-button.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+    });
+  
+    navLinks.forEach(link, index) => {
+        console.log(index);
+    }
+}
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+navSlide();
